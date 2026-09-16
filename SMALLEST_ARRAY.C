fc58163210b1d0bf7d57586f5a10db12640faf39
Rohini-km <rohinikm2008@gmail.com>
2026-09-16 19:57:09 +0530

    Add program to find the smallest number in an array
    smallest _array

---
+++ SMALLEST_ARRAY.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+inta[5] = {10,20,5,60,3};
+int smallest,i;
+smallest = a[0];
+for (i=1;i<5;i++)
+{
+  if(a[i]< smallest)
+  { 
+    smallest= a[i];
+  }
+}
+printf ("The smallest number is = %d",smallest);
+  return 0;
+}
+
+   
